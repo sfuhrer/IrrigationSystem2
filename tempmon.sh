ac3aa51e49b5c8b8f4e980cd16aebe9b2c1f7df8
Silvan Fuhrer <silvan.fuhrer@hotmail.com>
2021-02-21 19:57:39 +0000

    tempmon: wip for reading sensor data and writing it to file

--- tempmon.sh
+++ tempmon.sh
@@ -3,19 +3,33 @@
 timestamp=`date +%Y-%m-%d_%H-%M-%S`
 
 filename="log/"$timestamp".txt"
-#filename="test.txt"
 
 touch $filename
 echo  "Created file: "$filename
 
-printf "%-15s%5s\n" "Timestamp Temperature[°C]"
-printf "%20s\n" "------------------"
+#printf "%-15s%5s\n" "Timestamp Temperature[°C]"
+#printf "%20s\n" "------------------"
 
 while true
 do
-	temperature=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+	python drivers/bme280_read.py | {
+  		while IFS= read -r line
+  		do
+    			lastline="$line"
+  		done
+
+  		echo "Last measurements (temp-hum-pres): $lastline"
+	}
+
+	IFS='-' read -ra ADDR <<< "$lastline"
+	echo "lastline again:" "asfd"
+	for i in "${ADDR[@]}"; do
+    		echo "$i"
+	done
+
+	temp_pi=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
 	timestamp=`date +%Y-%m-%d_%H-%M-%S`
-	echo "$temperature $timestamp"
-	echo "$timestamp $temperature" >> $filename
-	sleep 60
+	echo "$temp_pi $timestamp"
+	echo "$timestamp $temp_pi" >> $filename
+	sleep 5
 done
