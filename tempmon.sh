4ef280e447a92de08d68996b676a484e318c1170
Silvan Fuhrer <silvan.fuhrer@hotmail.com>
2021-03-05 23:21:01 +0000

    tempmon: start new log when date changes

--- tempmon.sh
+++ tempmon.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-timestamp=$(date +%Y_%m_%d_%H_%M_%S)
-
-filename="/home/pi/IrrigationSystem2/log/"$timestamp".txt"
-#filename="test"
-
-touch $filename
-echo  "Created file: "$filename
-
-#printf "%-15s%5s\n" "Timestamp Temperature[°C]"
-#printf "%20s\n" "------------------"
-
+#i2cdetect needed, otherwise I2C connection is not detected by driver
 i2cdetect -y 1
 
 i_max=1440 #max iterations
 dt=60 #time step, seconds
 i=1
+date_prev="0"
+
+filename="test"
+
 while [ $i -le $i_max ]
 do  	
+	#check if new day, and open new file if so
+	timestamp=$(date +%Y_%m_%d_%H_%M_%S)
+	data_curr=${timestamp:0:10}
+
+	if [ "$date_curr" != "$data_prev" ];
+	then	
+		filename="/home/pi/IrrigationSystem2/log/"$timestamp".txt"
+		#filename="test"
+
+		touch $filename
+		echo  "Created file: "$filename
+	fi
+
 	output=$(python /home/pi/IrrigationSystem2/drivers/bme280_read.py)
 
+	#measurement data is separated by -
 	IFS='-' #setting - as delimiter
 	read -a strarr <<<"$output" #reading str as an array as tokens separated by IFS  
 	temp=${strarr[0]}
