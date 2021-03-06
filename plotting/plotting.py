3f2afed1cd21b1e2a964c4d1cf2f61ae43bfa3c2
sfuhrer <silvan.fuhrer@hotmail.com>
2021-03-06 14:08:18 +0100

    plotting: update to new timestamps, only plot temp and hum

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -2,11 +2,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime
+import matplotlib.dates as mdates
 
-with open('../log/2021_03_01_09_17_13.txt') as f:
+with open('../log/2021_03_06_09_59_33.txt') as f:
     lines = f.readlines()
 
     timestamp_full = [line.split()[0] for line in lines]
+    time_datetime = [datetime.strptime(d, '%Y-%m-%d_%H:%M:%S') for d in timestamp_full]
     # Time = [datetime.datetime.strptime(line[0], "%H%M%S%f") for line in I020]
     # Time1 = [mdates.date2num(line) for line in Time]
     temp = [float(line.split()[1]) for line in lines]
@@ -25,52 +27,34 @@ def timestamp_to_sec(timestamp):
     sec = int(timestamp[length - 2:])
     return hrs * 3600 + minutes * 60 + sec
 
-x_orig = ['2015-12-29 15:01:25', '2015-12-29 15:04:18']
-x_orig = ['2021_03_03_07_17_19 ', '2021_03_03_08_17_20 ']
-x = [datetime.strptime(d, '%Y-%m-%d-%H:%M:%S') for d in x_orig]
-y = ['7.1', '7.4', '9.4', '10.2']
 
-xs = matplotlib.dates.date2num(x)
-hfmt = matplotlib.dates.DateFormatter('%Y-%m-%d\n%H:%M:%S')
+xs = matplotlib.dates.date2num(time_datetime)
+# hfmt = matplotlib.dates.DateFormatter('%Y-%m-%d\n%H:%M:%S')
+hfmt = mdates.DateFormatter('%H:%M:%S')
 
 fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-ax.patch.set_facecolor('lightgrey')
-ax.xaxis.set_major_formatter(hfmt)
-ax.set_title('Titel des Reports')
-ax.set_xlabel('datum')
-ax.set_ylabel('2MTemperatur')
-plt.setp(ax.get_xticklabels(), size=8)
-ax.plot(xs, y, linewidth=2)
-ax.scatter(xs, y)
+
+ax1 = fig.add_subplot(211)
+ax1.patch.set_facecolor('lightgrey')
+ax1.xaxis.set_major_formatter(hfmt)
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Temp [°]')
+plt.setp(ax1.get_xticklabels(), size=8)
+ax1.plot(xs, temp, linewidth=2)
+ax1.xaxis.set_major_formatter(hfmt)
 plt.grid()
-plt.show()
 
+# add a subplot that shares the x-axis with ax1
+ax2 = plt.subplot(212, sharex=ax1)
+ax2.patch.set_facecolor('lightgrey')
+ax2.xaxis.set_major_formatter(hfmt)
+ax2.set_xlabel('Time')
+ax2.set_ylabel('Humidity [%]')
+plt.setp(ax2.get_xticklabels(), size=8)
+ax2.plot(xs, hum, linewidth=2)
+ax2.xaxis.set_major_formatter(hfmt)
 
-timestamp_s = []
-for i in timestamp_full:
-    timestamp_s.append(timestamp_to_sec(i))
-
-plt.subplot(2, 2, 1)
-plt.plot(timestamp_s, temp, 'o-')
-plt.title('Some logging')
-plt.ylabel('Temp [°C]')
-
+plt.gcf().autofmt_xdate()
 plt.tight_layout()
-
-plt.subplot(2, 2, 2)
-plt.plot(temp_ext, '.-')
-plt.xlabel('Time [s]')
-plt.ylabel('Temp ext')
-
-plt.subplot(2, 2, 3)
-plt.plot(timestamp_s, hum, '.-')
-plt.xlabel('Time [s]')
-plt.ylabel('Hum 2')
-
-plt.subplot(2, 2, 4)
-plt.plot(timestamp_s, pres, '.-')
-plt.xlabel('Time [s]')
-plt.ylabel('pres 2')
-
+plt.grid()
 plt.show()
