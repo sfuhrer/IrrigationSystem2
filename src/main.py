0393efebc9a41ee17946f05798db552d2cbe07c6
Silvan Fuhrer <silvan@auterion.com>
2021-07-22 23:00:06 +0200

    fix str
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/main.py
+++ src/main.py
@@ -39,7 +39,7 @@ while True:
 
     now = datetime.now()
     # every day at 08.00 start Pump controller
-    if (now.hour == 21 and now.minute == 58):
+    if (now.hour == 22 and now.minute == 1):
         #logger.log_message(f, "test")
         pumpResult = PumpController.Start(AverageTemp24)
         logger.log_message(f, pumpResult)
