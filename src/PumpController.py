1b2db2bfb2aa757dd2bcf57ee993e4f432f4f5b9
Silvan Fuhrer <silvan@auterion.com>
2021-07-22 22:56:45 +0200

    remove deg
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/PumpController.py
+++ src/PumpController.py
@@ -10,7 +10,7 @@ def Start(TempFilt24):
     else:
         PumpDuration = 100
 
-    str = "Pump controller started, average temp last 24h: " + str(TempFilt24) + "°C " + "Duration: " + str(PumpDuration) + "s"
+    str = "Pump controller started, average temp last 24h: " + str(TempFilt24) + " deg " + "Duration: " + str(PumpDuration) + "s"
     print(str)
     PumpDriver.RunPump(PumpDuration)
     return str
