76d5c345c7cc2d37262e39367e284127a8815ed2
Silvan Fuhrer <silvan@auterion.com>
2021-07-23 13:00:03 +0200

    adapt pumping durations

--- src/PumpController.py
+++ src/PumpController.py
@@ -6,9 +6,11 @@ import PumpDriver
 def Start(TempFilt24):
     
     if (TempFilt24 > 30):
-        PumpDuration = 200
+        PumpDuration = 360
+    elif(TempFilt24 > 20):
+        PumpDuration = 180
     else:
-        PumpDuration = 100
+        PumpDuration = 120
 
     feedbackString = "Pump controller started, average temp last 24h: " + format(TempFilt24, ".1f") + " deg " + "Duration: " + str(PumpDuration) + "s"
     print(feedbackString)
